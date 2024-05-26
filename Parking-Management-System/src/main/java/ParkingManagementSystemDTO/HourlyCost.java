26743484691485960ec40b2141ebe4b0b0a95635
Manish Kumar Singh <49101659+ms7644030@users.noreply.github.com>
2024-05-26 17:21:51 +0530

    Create HourlyCost.java

---
+++ Parking-Management-System/src/main/java/ParkingManagementSystemDTO/HourlyCost.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package ParkingManagementSystemDTO;
+
+import java.util.Map;
+
+/**
+ * 
+ */
+public class HourlyCost {
+	private Map<ParkingSpotType,Long> hourlyCost;
+
+	/**
+	 * @return the hourlyCost
+	 */
+	public Map<ParkingSpotType, Long> getHourlyCost() {
+		return hourlyCost;
+	}
+
+	/**
+	 * @param hourlyCost the hourlyCost to set
+	 */
+	public void setHourlyCost(Map<ParkingSpotType, Long> hourlyCost) {
+		this.hourlyCost = hourlyCost;
+	}
+
+}

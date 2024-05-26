5aad4adeb169ce40f3b5081522d0f04f902044d3
Neha <nehasinha280900@gmail.com>
2024-05-26 17:18:58 +0530

    POJO for entry panel and payment
    POJO for entry panel and payment

---
+++ Parking-Management-System/src/main/java/ParkingManagementSystemDTO/EntryPanel.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package ParkingManagementSystemDTO;
+
+/**
+ * 
+ */
+public class EntryPanel {
+private String entryPanelId;
+
+/**
+ * @return the entryPanelId
+ */
+public String getEntryPanelId() {
+	return entryPanelId;
+}
+
+/**
+ * @param entryPanelId the entryPanelId to set
+ */
+public void setEntryPanelId(String entryPanelId) {
+	this.entryPanelId = entryPanelId;
+}
+
+}

fd8097cd756738665c781eb18630fd97218d87a7
Manish Kumar Singh <49101659+ms7644030@users.noreply.github.com>
2024-06-01 16:24:45 +0530

    Entity - Cost_Details

--- Parking-Management-System/src/main/java/ParkingManagementSystemEntities/CostDetails.java
+++ Parking-Management-System/src/main/java/ParkingManagementSystemEntities/CostDetails.java
@@ -1,5 +1,22 @@
 package ParkingManagementSystemEntities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Cost_Details")
+@Data
+@NoArgsConstructor
 public class CostDetails {
+	@Id
+	@Column(name="Vehicle_Type", length = 10)
+	private String vehicleType;
+	@Column(name = "Price_per_Hours", length = 10)
+	private Long pricePerHrs;
+	
 
 }

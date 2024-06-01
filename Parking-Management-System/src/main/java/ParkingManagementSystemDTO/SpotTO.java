28c8cb1cfebdabed1e53dc75aec4ad41e6810806
Heviansh <heviansh@gmail.com>
2024-06-01 16:59:38 +0530

    Entity @ Spot.java

---
+++ Parking-Management-System/src/main/java/ParkingManagementSystemDTO/SpotTO.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ParkingManagementSystemDTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ */
+@Data
+@NoArgsConstructor
+public class SpotTO {
+	private String spotId;
+	private Long floorId;
+	private String spotType;
+	
+
+}
--- Parking-Management-System/src/main/java/ParkingManagementSystemEntities/Spot.java
+++ Parking-Management-System/src/main/java/ParkingManagementSystemEntities/Spot.java
@@ -1,5 +1,23 @@
 package ParkingManagementSystemEntities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Spot")
+@NoArgsConstructor
+@Data
 public class Spot {
+	@Id
+	@Column(name="Spot_Id", length = 10)
+	private String spotId;
+	@Column(name = "Floor_Id", length = 10)
+	private Long floorId;
+	@Column(name="Spot_Type", length = 20)
+	private String spotType;
 
 }

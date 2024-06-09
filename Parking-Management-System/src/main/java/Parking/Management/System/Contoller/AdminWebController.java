a8e97568685cabfa8b9baafbb89fa49c649095f0
Heviansh <heviansh@gmail.com>
2024-06-09 13:50:47 +0530

    Modified Controller (Added @CrossOrigin (origins="*")

--- Parking-Management-System/src/main/java/Parking/Management/System/Contoller/AdminWebController.java
+++ Parking-Management-System/src/main/java/Parking/Management/System/Contoller/AdminWebController.java
@@ -3,6 +3,7 @@ package Parking.Management.System.Contoller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import Parking.Management.System.Entities.Admin;
 import Parking.Management.System.Service.ParkingManagementServiceImp;
 
+@CrossOrigin (origins="*")
 @RestController
 @RequestMapping(path = "/pms")
 public class AdminWebController {
